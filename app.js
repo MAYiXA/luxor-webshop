928200dc91dce713881d7e28df2f1c13c100b579
Emma <emmaborghult@gmail.com>
2020-02-24 14:16:17 +0100

    Complete payment form+add validation

---
+++ app.js
@@ -0,0 +1,4 @@
+// fanns ingen möjlighet att få radio-knappar obligatoriska via HTML endast så fick använda js
+document.getElementById('pay-later').required = true;
+document.getElementById('cards').required = true;
+document.getElementById('split-pay').required = true;
